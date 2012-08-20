3be145c3bee7a64cc8b3d96cf441a04897a864f6
ivanbrazza <dudeman1996@gmail.com>
2012-08-20 12:26:57 -0400

    Initial commit

---
+++ products/pa_marvel.mk
@@ -0,0 +1,18 @@
+# Check for target product
+ifeq (pa_marvel,$(TARGET_PRODUCT))
+ 
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := HDPI
+   
+# include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# Inherit CM9 device configuration
+include $(call all-subdir-makefiles)
+$(call inherit-product, device/htc/marvel/cm.mk)
+    
+PRODUCT_NAME := pa_marvel
+    
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+    
+endif
