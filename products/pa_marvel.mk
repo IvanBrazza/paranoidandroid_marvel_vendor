47b0336d46068a5223b2e7f1952048abac7bbb9d
Ivan <dudeman1996@gmail.com>
2012-11-06 20:45:28 +0000

    Update products/pa_marvel.mk

--- products/pa_marvel.mk
+++ products/pa_marvel.mk
@@ -1,14 +1,13 @@
 # Check for target product
 ifeq (pa_marvel,$(TARGET_PRODUCT))
  
-# Define PA bootanimation size
-PARANOID_BOOTANIMATION_NAME := MDPI
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_mdpi
    
 # include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
 
-# Inherit CM9 device configuration
-include $(call all-subdir-makefiles)
+# Inherit CM device configuration
 $(call inherit-product, device/htc/marvel_pa/cm.mk)
     
 PRODUCT_NAME := pa_marvel
